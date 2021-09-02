67bda603de3e1f805f8df11cf514bf4ffe176295
NIKHILA30459 <84761728+NIKHILA30459@users.noreply.github.com>
2021-09-02 19:12:21 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,12 +1,10 @@
-# Python3 program to add two numbers
-  
-number1 = input("First number: ")
-number2 = input("\nSecond number: ")
-  
-# Adding two numbers
-# User might also enter float numbers
-sum = float(number1) + float(number2)
-  
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
 # Display the sum
-# will print value in float
-print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
